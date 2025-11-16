1f5f27b51654bb735485dbc391115cae223fcf8c
Nexdevjnr <contact.leokha@gmail.com>
2025-11-16 02:13:38 +0100

    integration home page and moon destintion page (mobile)

--- public/script.js
+++ public/script.js
@@ -1,6 +1,11 @@
 const navBtn = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
+const close = document.querySelector(".close-menu")
 
 navBtn.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+close.addEventListener("click", () => {
+  navLinks.classList.remove("active")
+})
