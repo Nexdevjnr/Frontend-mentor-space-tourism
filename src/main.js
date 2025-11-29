6d1d62adf2fe28a8ae038c4aff4f1cbcaf06dc63
Leo <contact.leokha@gmail.com>
2025-11-29 22:48:18 +0100

    finition: add animation en fix font and details + readme

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-const closeMenu = document.querySelector('.close-menu');
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const closeMenu = document.querySelector(".close-menu");
+const overlay = document.querySelector(".overlay");
 
 function toggleMenu() {
-  navLinks.classList.toggle('active');
-  
-  if (navLinks.classList.contains('active')) {
-    document.body.style.overflow = 'hidden';
+  navLinks.classList.toggle("active");
+  overlay.classList.toggle("active");
+
+  if (navLinks.classList.contains("active")) {
+    document.body.style.overflow = "hidden";
   } else {
-    document.body.style.overflow = 'auto';
+    document.body.style.overflow = "auto";
   }
 }
 
-hamburger.addEventListener('click', toggleMenu);
-closeMenu.addEventListener('click', toggleMenu);
+hamburger.addEventListener("click", toggleMenu);
+closeMenu.addEventListener("click", toggleMenu);
+overlay.addEventListener("click", toggleMenu);
